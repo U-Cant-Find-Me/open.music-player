94d35237b7ac7fd0c997746e985035cfcac5af7c
U-Cant-Find-Me <anirbanmoi.nitdgp.19u10573@gmail.com>
2023-08-24 18:19:10 +0530

    Loader Styles

--- app/(site)/loading.tsx
+++ app/(site)/loading.tsx
@@ -1,14 +1,15 @@
-'use client';
+"use client";
 
+import { ScaleLoader } from "react-spinners";
 import Box from "@/components/Box";
-import { BounceLoader } from 'react-spinners';
 
 const Loading = () => {
     return (
-        <Box className="h-full flex items-center justify-center">
-            <BounceLoader color="#22c55e" size={40} />
+        <Box className="h-full flex items-center justify-center gap-x-3">
+            <ScaleLoader color="#00ff82" height={60} loading margin={4} radius={3} width={5} />
+            <ScaleLoader color="#00ff82" height={60} loading margin={4} radius={3} width={5} />
         </Box>
-    )
+    );
 }
 
 export default Loading;
--- app/account/loading.tsx
+++ app/account/loading.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { BounceLoader } from "react-spinners";
-
+import { ScaleLoader } from "react-spinners";
 import Box from "@/components/Box";
 
 const Loading = () => {
-  return ( 
-    <Box className="h-full flex items-center justify-center">
-      <BounceLoader color="#22c55e" size={40} />
-    </Box>
-  );
+    return (
+        <Box className="h-full flex items-center justify-center gap-x-3">
+            <ScaleLoader color="#00ff82" height={60} loading margin={4} radius={3} width={5} />
+            <ScaleLoader color="#00ff82" height={60} loading margin={4} radius={3} width={5} />
+        </Box>
+    );
 }
- 
+
 export default Loading;
